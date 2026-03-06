3c9879b3b30a7e46bcc142da41e1427316f0a3a2
Sai Venkat <saivenkatkankipati456@gmail.com>
2026-03-06 19:08:25 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, 
-    allowedHosts:"all",
-    port: 3000, 
+    host: true,
+    port: 3000,
+    allowedHosts: [
+      "multi-tenant-saas-platform-2-jg8g.onrender.com"
+    ],
     watch: {
-      usePolling: true 
+      usePolling: true
     }
   }
 })
